6d7532c3247b3e8b3072b1c2a995e2bdf974abe2
whummy <aparaolawunmi@gmail.com>
2024-06-15 22:28:39 +0100

    creates flight and hotel models

---
+++ Domain/Flight.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Flight
+    {
+        public int Id { get; set; }
+        public string? Airline { get; set; }
+        public string? From { get; set; }
+        public string? To { get; set; }
+        public decimal Price { get; set; }
+        public DateTime? DepartureDate { get; set; }
+    }
+}
